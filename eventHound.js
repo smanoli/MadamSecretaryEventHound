5b66624089d4aa9d85e6e804745fcac18b4bac0c
smanoli <smanoli@MacBook-Air.attlocal.net>
2016-10-02 15:39:45 -0700

    dvc phone banks parameterized

--- eventHound.js
+++ eventHound.js
@@ -1,3 +1,18 @@
+// Usage:
+// // query string: ?foo=lorem&bar=&baz
+// var foo = getParameterByName('foo'); // "lorem"
+// var bar = getParameterByName('bar'); // "" (present with empty value)
+// var baz = getParameterByName('baz'); // "" (present with no value)
+// var qux = getParameterByName('qux'); // null (absent)
+function getParameterByName(name, url){
+	if (!url) url = window.location.href;
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+		results = regex.exec(url);
+if (!results) return null;
+if (!results[2]) return '';
+return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
 function formatHours(hours) {
 	return (hours > 12) ? hours - 12 : hours;
 };
@@ -73,7 +88,6 @@ function getLocString(event){
 	return locLine;
 }
 function readResponse(response){
-
 };
 function addEvent(event, section){
 	var eventName = event.name;
